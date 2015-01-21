a8c9e6d161750d39ae044b2abc49123ab0fd95b8
ivamacio <ivan4341@gmail.com>
2015-01-21 10:09:30 -0500

    ivamacio's commit

---
+++ test-ios/test-ios/IMTest.h
@@ -0,0 +1,13 @@
+//
+//  IMTest.h
+//  test-ios
+//
+//  Created by Ivamácio Magalhães on 2015-01-21.
+//  Copyright (c) 2015 Yao Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IMTest : NSObject
+
+@end
---
+++ test-ios/test-ios/IMTest.m
@@ -0,0 +1,13 @@
+//
+//  IMTest.m
+//  test-ios
+//
+//  Created by Ivamácio Magalhães on 2015-01-21.
+//  Copyright (c) 2015 Yao Lu. All rights reserved.
+//
+
+#import "IMTest.h"
+
+@implementation IMTest
+
+@end
