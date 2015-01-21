c53073aa0c2dd3a22d46e5c25ed9ec1428151bc4
chinmaykumargajjar <gajjar.chinmay@gmail.com>
2015-01-21 10:01:48 -0500

    Created the class.

---
+++ test-ios/ChinmayGajjar.h
@@ -0,0 +1,13 @@
+//
+//  ChinmayGajjar.h
+//  test-ios
+//
+//  Created by Chinmaykumar Gajjar on 1/21/15.
+//  Copyright (c) 2015 Yao Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChinmayGajjar : UIView
+
+@end
---
+++ test-ios/ChinmayGajjar.m
@@ -0,0 +1,21 @@
+//
+//  ChinmayGajjar.m
+//  test-ios
+//
+//  Created by Chinmaykumar Gajjar on 1/21/15.
+//  Copyright (c) 2015 Yao Lu. All rights reserved.
+//
+
+#import "ChinmayGajjar.h"
+
+@implementation ChinmayGajjar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
