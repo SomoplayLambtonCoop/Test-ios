691c539322a2c6aa26d50445251f1b1fd02c5cc0
GANESH GHEEWALA <macadmin@Administrators-iMac-133.local>
2015-01-21 10:48:10 -0500

    two files added by ganesh

---
+++ test-ios/Test-ios/ganesh.h
@@ -0,0 +1,13 @@
+//
+//  ganesh.h
+//  test-ios
+//
+//  Created by macadmin on 2015-01-21.
+//  Copyright (c) 2015 Yao Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ganesh : NSObject
+
+@end
---
+++ test-ios/Test-ios/ganesh.m
@@ -0,0 +1,13 @@
+//
+//  ganesh.m
+//  test-ios
+//
+//  Created by macadmin on 2015-01-21.
+//  Copyright (c) 2015 Yao Lu. All rights reserved.
+//
+
+#import "ganesh.h"
+
+@implementation ganesh
+
+@end
